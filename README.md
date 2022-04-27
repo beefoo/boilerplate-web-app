0f8d7223389c0d9a1914a94552faee573be22c3d
beefoo <brian@youaremyjoy.org>
2022-04-26 22:15:20 -0400

    add server.js, update dependencies, update eslint rules

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# boilerplate-web-app
-My personal boilerplate for starting web apps
+# Boilerplate Web App
+
+My personal boilerplate for starting web apps.
+
+## Checklist
+
+1. Fill out title and meta in head of html
+2. Update package.json
+3. Add icons
+4. Check for updated third-party libraries
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+let port = 2222;
+const app = express();
+
+if (process.argv.length > 2) port = parseInt(process.argv[2], 10);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./'));
+app.listen(port, () => console.log(`Listening on port ${port}`));
